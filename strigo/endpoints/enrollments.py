8e533f2775d39d3911495746e3df1ef33191ba03
nir0s <nir36g@gmail.com>
2019-08-07 12:22:55 +0300

    FEAT: Supporting patching enrollments

--- strigo/endpoints/enrollments.py
+++ strigo/endpoints/enrollments.py
@@ -18,6 +18,11 @@ class Enrollments:
 
         return self.client.post(path, body=body)
 
+    def patch(self, course_id, enrollment_id, body):
+        path = utils.join_url(self.base, enrollment_id, course_id=course_id)
+
+        return self.client.patch(path, body=body)
+
     def delete(self, course_id, enrollment_id):
         path = utils.join_url(self.base, enrollment_id, course_id=course_id)
 
